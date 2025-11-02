bc2ef02316967090f9a98f2e5c474e27fe753224
herman-xphp <hermanxphp@gmail.com>
2025-11-02 11:41:59 +0800

    Environtment Variable

--- viper_test.go
+++ viper_test.go
@@ -64,3 +64,24 @@ func TestENVFile(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
 	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
 }
+
+func TestENV(t *testing.T) {
+	config := viper.New()
+	// config.SetConfigName("config")
+	// config.SetConfigType("yaml")
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+	config.AutomaticEnv()
+
+	// read config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("APP_NAME"))
+	assert.Equal(t, "Muslim Dev", config.GetString("APP_AUTHOR"))
+	assert.Equal(t, "localhost", config.GetString("DATABASE_HOST"))
+	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
+	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
+
+	assert.Equal(t, "Hello", config.GetString("FROM_ENV"))
+}
